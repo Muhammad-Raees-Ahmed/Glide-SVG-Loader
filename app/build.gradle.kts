1bfd2a92ced3aab5764c8e88becae902e3e33630
Muhammad Raees Ahmed <muhammadraeesahmed38@gmail.com>
2025-03-03 22:51:21 -0800

    setup gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -45,4 +46,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // add these libraries for svg image loading from remote/local
+    implementation(libs.glide)
+    //noinspection KaptUsageInsteadOfKsp
+    kapt(libs.compiler)
+    implementation (libs.androidsvg)
 }
