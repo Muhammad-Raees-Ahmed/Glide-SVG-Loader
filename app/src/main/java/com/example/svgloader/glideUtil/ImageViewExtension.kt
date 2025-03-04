584f28bbf84ee7cde75c0a08c7c9741159ff93e5
Muhammad Raees Ahmed <muhammadraeesahmed38@gmail.com>
2025-03-03 23:05:04 -0800

    add extension function for image loading

---
+++ app/src/main/java/com/example/svgloader/glideUtil/ImageViewExtension.kt
@@ -0,0 +1,14 @@
+package com.example.svgloader.glideUtil
+
+import android.graphics.drawable.PictureDrawable
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+fun ImageView.loadSvg(url: String) {
+    Glide.with(this.context)
+        .`as`(PictureDrawable::class.java)
+        .apply(RequestOptions().centerCrop())
+        .load(url)
+        .into(this)
+}
